e3c5e7e59fb8f53fb800c205e8ab0f1be6022155
Sérgio Gabriel <nihhiu17@gmail.com>
2024-01-20 17:01:54 +0000

    Correção de erros, PRISMA 1 GISUPD 1

--- src/daemon/gis-updater/main.py
+++ src/daemon/gis-updater/main.py
@@ -1,31 +1,34 @@
 import pika
 import sys
 import time
+import logging
 
 POLLING_FREQ = int(sys.argv[1]) if len(sys.argv) >= 2 else 60
 ENTITIES_PER_ITERATION = int(sys.argv[2]) if len(sys.argv) >= 3 else 10
 
+logging.basicConfig(level=logging.DEBUG)
+
 def callback(ch, method, properties, body):
     print(f"Received a delayed message: {body}")
-    # TODO: Adicione o código para processar a mensagem conforme necessário
-    # Por exemplo, você pode executar as etapas 1-3 mencionadas nos comentários do seu script Python.
+    # TODO: Add the code to process the message as needed
 
 if __name__ == "__main__":
-    connection = pika.BlockingConnection(pika.ConnectionParameters('broker', virtual_host='is', credentials=pika.PlainCredentials('is', 'is')))
-    channel = connection.channel()
-    channel.queue_declare(queue='update-gis_counties_queue', durable=True)
-    channel.basic_consume(queue='update-gis_counties_queue', on_message_callback=callback, auto_ack=True)
-    
-    print('Waiting for delayed messages. To exit press CTRL+C')
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', port=5672, virtual_host='is', credentials=pika.PlainCredentials('is', 'is')))
+        channel = connection.channel()
+        channel.queue_declare(queue='update-gis_counties_queue', durable=True)
+        channel.basic_consume(queue='update-gis_counties_queue', on_message_callback=callback, auto_ack=True)
 
-    while True:
-        # Adicione aqui a lógica para recuperar entidades sem coordenadas
-        print(f"Getting up to {ENTITIES_PER_ITERATION} entities without coordinates...")
-        # !TODO: 1- Use api-gis to retrieve a fixed amount of entities without coordinates (e.g. 100 entities per iteration, use ENTITIES_PER_ITERATION)
-        # !TODO: 2- Use the entity information to retrieve coordinates from an external API
-        # !TODO: 3- Submit the changes
+        print('Waiting for delayed messages. To exit press CTRL+C')
 
-        # Aguarde o tempo de intervalo antes da próxima iteração
-        time.sleep(POLLING_FREQ)
+        while True:
+            # Add logic to retrieve entities without coordinates
+            print(f"Getting up to {ENTITIES_PER_ITERATION} entities without coordinates...")
 
-    channel.start_consuming()
+            # Wait for the polling frequency before the next iteration
+            time.sleep(POLLING_FREQ)
+
+        channel.start_consuming()
+
+    except pika.exceptions.AMQPConnectionError as e:
+        print(f"Error connecting to RabbitMQ: {e}")
